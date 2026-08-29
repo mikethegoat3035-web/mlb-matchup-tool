675a8efa8967228136985f3badce29e7ad00bcba
mikethegoat3035-web <mikethegoat3035@gmail.com>
2026-08-29 10:51:20 -0500

    Update prop_model_combined.py

--- prop_model_combined.py
+++ prop_model_combined.py
@@ -9496,3 +9496,4 @@ def backtest_pitcher_win_walk_forward(pitcher_id: int, season: int,
             "hit": predicted_over == actual_over,
         })
     return pd.DataFrame(rows)
+                                            
